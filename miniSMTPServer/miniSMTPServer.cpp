2966b30d12215d5a873fa2825a24b4f8c5526fb7
shejialuo <shejialuo@gmail.com>
2023-03-12 17:09:58 +0800

    feat: add a simple tcp server

--- miniSMTPServer/miniSMTPServer.cpp
+++ miniSMTPServer/miniSMTPServer.cpp
@@ -1,6 +1,30 @@
+#include "socket.hpp"
+
 #include <iostream>
+#include <string>
 
 int main() {
   std::cout << "Hello, This is a simple SMTP server\n";
+
+  TCPSocket socket{};
+  socket.bind();
+  socket.listen();
+
+  std::string result{};
+
+  while (true) {
+    auto s = socket.accept();
+
+    while (true) {
+      s.read(result);
+      if (result.size() == 0) {
+        std::cout << "S: Connection lost\n";
+        s.close();
+        break;
+      }
+      s.write("Hello, this is the server\n");
+    }
+  }
+
   return 0;
 }
